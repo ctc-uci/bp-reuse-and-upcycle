e33e384b2740ff55ac5f9c14858dcd5508640f1e
Ethan So <ethanso@ethans-air.lan>
2022-02-04 17:35:55 -0800

    Played with formatting on about page

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,27 @@ function About() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>About Wasteless:</p>
+        <h3 className="aboutPage">About Wasteless:</h3>
+        {/* who are we? */}
+        <p className="aboutPage">
+          Wasteless is a mobile application designed and developed by students at UCI.
+        </p>
+        {/* what this app about  */}
+
+        <h3 className="aboutPage">Our Mission: </h3>
+        <p className="aboutPage">
+          This project started out as an interest in environmental sustainability.
+          <br />
+          We wanted to create something that would help users find recycling locations
+          <br />
+          so that they could reduce their overall waste.
+        </p>
+
+        {/* team members + pictures */}
+        <p>pictures</p>
+
+        {/* contact */}
+        <p>contact info:</p>
       </header>
     </div>
   );
